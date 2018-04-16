09121f751047b5bcbe148cd806bf6fd13b64f305
LW-Ho <leoleo41306@gmail.com>
2018-04-16 21:37:13 +0800

    update the code.

---
+++ flips_card/PlayCardDeck.swift
@@ -0,0 +1,29 @@
+//
+//  PlayCardDeck.swift
+//  flips_card
+//
+//  Created by White on 2018/4/16.
+//  Copyright © 2018年 White. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all{
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4Random)
+        }else {
+            return nil;
+        }
+    }
+}
+
--- flips_card/ViewController.swift
+++ flips_card/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print ("\(card)");
+            } else {
+                print ("Deck draw Error ... ")
+            }
+        }
+    }
 
     //var game = Concentration(numberOfPairsCards: 3)
     private lazy var game = Concentration(numberOfPairsCards: (numberOfPairsOfCardsOnTable))
